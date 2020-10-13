1a3ad46e45b544ec79b48e34e610facd1e9a0544
Arkadiusz Kołodziejek <arkko002@gmail.com>
2020-10-13 12:40:27 +0200

    Add AbstractBannerGrabber, HTTPGrabber, stub of BannerResult

---
+++ richMap/port_scanning/banner_grabber/HTTPGrabber.py
@@ -0,0 +1,17 @@
+import socket
+from scapy.layers.http import http_request
+
+from port_scanning.banner_grabber.abstract_banner_grabber import AbstractBannerGrabber
+
+
+class HTTPGrabber(AbstractBannerGrabber):
+    def __init__(self, target):
+        super(HTTPGrabber, self).__init__(target)
+        self.hostname = self._get_hostname()
+
+    def get_banner(self):
+        response = http_request(self.hostname)
+        return response.server
+
+    def _get_hostname(self):
+        return socket.gethostbyaddr(self.target)[0]
