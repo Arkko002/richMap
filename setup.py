012a19c67524a63321ba03e40251289e7a059b55
Arkadiusz Kołodziejek <arkko002@gmail.com>
2021-01-14 22:57:37 +0100

    Add packages field in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="RichMap",
     version="0.1",
     py_modules=["rich_map"],
+    packages=find_packages(exclude=("test",)),
     install_requires=[
         "Click",
         "Scapy"
